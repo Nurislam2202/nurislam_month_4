664ea2871b51a8cc5883edfe071ee1ddbf5ed916
Nurislam2202 <nurislamuraimov2202@gmail.com>
2024-12-22 17:01:51 +0600

    'lesson2'

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+from posts.models import Post
+
+
+def main_view(request):
+    return render(request, "base.html")
 
 
 def hello_view(request):
-    return HttpResponse('Hello World')
+    return HttpResponse(f"Hello World {random.randint(1, 100)}")
 
 
 def html_view(request):
     return render(request, 'base.html')
+
+
+def posts_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_view.html", context={"posts": posts})
